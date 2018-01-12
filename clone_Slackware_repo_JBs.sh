c494580500f65f57b1e19dad340d715e27e2c0c6
João Batista <joao42lbatista@gmail.com>
2018-01-12 00:55:36 -0200

    clone_Slackware_repo_JBs.sh - "\n" to "\\n" - "\t" to "\\t" - Add "create local md5sum only if wanted"

--- clone_Slackware_repo_JBs.sh
+++ clone_Slackware_repo_JBs.sh
@@ -22,13 +22,13 @@
 #
 # Script: Clone some Slackware repository to a local source
 #
-# Last update: 12/08/2017
+# Last update: 12/01/2018
 #
 # Tip: Use the file inside one "old" ISO to make less things to download
 #
 input1=$1
 if [ "$input1" == "noColor" ]; then
-    echo -e "\nColors disabled"
+    echo -e "\\nColors disabled"
 else
     # Some colors for script output - Make it easier to follow
     BLACK='\e[1;30m'
@@ -42,30 +42,30 @@ else
 fi
 
 if [ "$input1" == "testColor" ]; then
-    echo -e "\n\tTest colors: $RED RED $WHITE WHITE $PINK PINK $BLACK BLACK $BLUE BLUE $GREEN GREEN $CYAN CYAN $NC NC\n"
+    echo -e "\\n\\tTest colors: $RED RED $WHITE WHITE $PINK PINK $BLACK BLACK $BLUE BLUE $GREEN GREEN $CYAN CYAN $NC NC\\n"
 fi
 
 mirrorSource="ftp://ftp.osuosl.org/.2/slackware"
-echo -e "$CYAN\nDefault mirror:$GREEN $mirrorSource$NC"
+echo -e "$CYAN\\nDefault mirror:$GREEN $mirrorSource$NC"
 
-echo -en "$CYAN\nWant change the mirror?$NC\n(y)es - (n)o $GREEN(press enter to no):$NC "
+echo -en "$CYAN\\nWant change the mirror?$NC\\n(y)es - (n)o $GREEN(press enter to no):$NC "
 read -r changeMirror
 
 if [ "$changeMirror" == 'y' ]; then
     mirrorSource=''
 
     while echo "$mirrorSource" | grep -v -q -E "ftp|http"; do
-        echo -en "$CYAN \nType the new mirror:$NC "
+        echo -en "$CYAN \\nType the new mirror:$NC "
         read -r mirrorSource
 
         if echo "$mirrorSource" | grep -v -q -E "ftp|http"; then
-            echo -e "$RED\nError: the mirror \"$mirrorSource\" is not valid.\nOne valid mirror has \"ftp\" or \"http\"$NC"
+            echo -e "$RED\\nError: the mirror \"$mirrorSource\" is not valid.\\nOne valid mirror has \"ftp\" or \"http\"$NC"
         fi
     done
-    echo -e "$CYAN\nNew mirror:$GREEN $mirrorSource$NC"
+    echo -e "$CYAN\\nNew mirror:$GREEN $mirrorSource$NC"
 fi
 
-echo -en "$CYAN\nWith version Slackware you want? $GREEN(press enter to 14.2):$NC "
+echo -en "$CYAN\\nWith version Slackware you want? $GREEN(press enter to 14.2):$NC "
 read -r versionSlackware
 
 if [ "$versionSlackware" == '' ]; then
@@ -73,19 +73,19 @@ if [ "$versionSlackware" == '' ]; then
 fi
 
 if echo "$versionSlackware" | grep -qv "current"; then
-    echo -e "\n\t$RED#--------------------------------------------------------------------------#"
-    echo -en "$CYAN\t# Downlad only the patches (patches/)? (y)es - (n)o $GREEN(press enter to yes):$NC "
+    echo -e "\\n\\t$RED#--------------------------------------------------------------------------#"
+    echo -en "$CYAN\\t# Downlad only the patches (patches/)? (y)es - (n)o $GREEN(press enter to yes):$NC "
     read -r onlyPatches
 
     if [ "$onlyPatches" == '' ] || [ "$onlyPatches" == 'y' ]; then
         onlyPatches='y'
-        echo -en "\n$BLUE# Downloading only the patches #\n$NC"
+        echo -en "\\n$BLUE# Downloading only the patches #\\n$NC"
     else
-        echo -en "\n$BLUE# Downloading all the files #\n$NC"
+        echo -en "\\n$BLUE# Downloading all the files #\\n$NC"
     fi
 fi
 
-echo -en "$CYAN\nWith arch you want?$NC\n(1) - 32 bits or (2) - 64 bits $GREEN(press enter to 64 bits):$NC "
+echo -en "$CYAN\\nWith arch you want?$NC\\n(1) - 32 bits or (2) - 64 bits $GREEN(press enter to 64 bits):$NC "
 read -r choosedArch
 
 if [ "$choosedArch" == '1' ]; then
@@ -96,10 +96,10 @@ fi
 
 versionDownload=slackware$choosedArch-$versionSlackware
 
-echo -en "$CYAN\nWant download the source code?$NC\n(y)es - (n)o $GREEN(press enter to no):$NC "
+echo -en "$CYAN\\nWant download the source code?$NC\\n(y)es - (n)o $GREEN(press enter to no):$NC "
 read -r downloadSource
 
-echo -en "$CYAN\nWill download (by lftp) $GREEN\"$versionDownload\"$CYAN"
+echo -en "$CYAN\\nWill download (by lftp) $GREEN\"$versionDownload\"$CYAN"
 if [ "$downloadSource" == 'y' ]; then
     echo -en "$RED with $CYAN"
 else
@@ -107,11 +107,11 @@ else
 fi
 echo -e "the$BLUE source code$CYAN from $GREEN\"$mirrorSource\"$NC"
 
-echo -en "$CYAN\nWant continue?$NC\n(y)es - (n)o $GREEN(press enter to yes):$NC "
+echo -en "$CYAN\\nWant continue?$NC\\n(y)es - (n)o $GREEN(press enter to yes):$NC "
 read -r contineLftp
 
 if [ "$contineLftp" == 'n' ]; then
-    echo -e "$CYAN\nJust exiting by user choice$NC\n"
+    echo -e "$CYAN\\nJust exiting by user choice$NC\\n"
 else
     if [ "$downloadSource" != 'y' ]; then
         removeSoure="-x source/ -x patches/source/ -x /pasture/source/"
@@ -124,10 +124,10 @@ else
     fi
 
     if [ -e $versionDownload/ ]; then
-        echo -e "$CYAN\nOlder folder download found ($GREEN$versionDownload/$CYAN)$NC"
+        echo -e "$CYAN\\nOlder folder download found ($GREEN$versionDownload/$CYAN)$NC"
 
-        echo -en "$CYAN\nDownloading$BLUE ChangeLog.txt$CYAN to make a$BLUE fast check$CYAN (the$BLUE local$GREEN "
-        echo -e "ChangeLog.txt$CYAN with the$BLUE server$GREEN ChangeLog.txt$CYAN)$NC. Please wait...\n"
+        echo -en "$CYAN\\nDownloading$BLUE ChangeLog.txt$CYAN to make a$BLUE fast check$CYAN (the$BLUE local$GREEN "
+        echo -e "ChangeLog.txt$CYAN with the$BLUE server$GREEN ChangeLog.txt$CYAN)$NC. Please wait...\\n"
         wget "$mirrorSource"/"$versionDownload"/ChangeLog.txt -O ChangeLog.txt
 
         cd "$versionDownload" || exit
@@ -138,20 +138,20 @@ else
 
         changeLogMd5sumResult=$(echo -e "$checkChangeLogMd5sum" | awk '{print $2}')
 
-        echo -en "$CYAN\nThe$BLUE ChangeLog.txt$CYAN in the server is"
+        echo -en "$CYAN\\nThe$BLUE ChangeLog.txt$CYAN in the server is"
         contineOrJump='y'
         if [ "$changeLogMd5sumResult" == "OK" ]; then
             rm ChangeLog.txt
 
             echo -e "$GREEN equal$CYAN with the$BLUE ChangeLog.txt$CYAN in local folder"
-            echo -e "\n\t$RED#-----------------------------------------------------------#"
-            echo -e "$CYAN\t# Want continue/force the download or jump the download step?"
-            echo -en "$NC\t# (y)es to continue - (n)o to jump $GREEN(press enter to no):$NC "
+            echo -e "\\n\\t$RED#-----------------------------------------------------------#"
+            echo -e "$CYAN\\t# Want continue/force the download or jump the download step?"
+            echo -en "$NC\\t# (y)es to continue - (n)o to jump $GREEN(press enter to no):$NC "
             read -r contineOrJump
 
         else # $changeLogMd5sumResult == FAILED
             echo -e "$RED different$CYAN from the$BLUE ChangeLog.txt$CYAN in local folder$NC"
-            echo -en "$CYAN\nWant view the diff between these files?$NC\n(y)es - (n)o $GREEN(press enter to yes):$NC "
+            echo -en "$CYAN\\nWant view the diff between these files?$NC\\n(y)es - (n)o $GREEN(press enter to yes):$NC "
             read -r diffChangLog
 
             if [ "$diffChangLog" != 'n' ]; then
@@ -161,23 +161,30 @@ else
             rm ChangeLog.txt
         fi
 
+        echo -en "$CYAN\\nCreate a md5sum for all local files (can take a while)? $NC\\n(y)es or (n)o $GREEN(press enter no):$NC "
+        read -r useMd5sumCheckBeforeDownload
+
         if [ "$contineOrJump" == 'y' ]; then
-            tmpMd5sumBeforeDownload=$(mktemp)
+            if [ "$useMd5sumCheckBeforeDownload" == 'y' ]; then
+                tmpMd5sumBeforeDownload=$(mktemp)
 
-            listOfFilesBeforeDownload=$(find $versionDownload/ -type f -print)
+                listOfFilesBeforeDownload=$(find $versionDownload/ -type f -print)
 
-            echo -en "$CYAN\nCreating a$BLUE md5sum$RED (before the download)$CYAN from files found (in the folder $GREEN$versionDownload/$CYAN)$NC. Please wait..."
-            for file in $listOfFilesBeforeDownload; do
-                md5sum "$file" >> "$tmpMd5sumBeforeDownload"
-            done
-            echo -e "$CYAN\n\nThe$BLUE md5sum$RED (before the download)$CYAN was saved in the tmp file: $GREEN$tmpMd5sumBeforeDownload$NC"
+                echo -en "$CYAN\\nCreating a$BLUE md5sum$RED (before the download)$CYAN from files found (in the folder $GREEN$versionDownload/$CYAN)$NC. Please wait..."
+                for file in $listOfFilesBeforeDownload; do
+                    md5sum "$file" >> "$tmpMd5sumBeforeDownload"
+                done
+                echo -e "$CYAN\\n\\nThe$BLUE md5sum$RED (before the download)$CYAN was saved in the tmp file: $GREEN$tmpMd5sumBeforeDownload$NC"
+            else
+                tmpMd5sumBeforeDownload=''
+            fi
         fi
     else
         contineOrJump='y'
     fi
 
     if [ "$contineOrJump" == 'y' ]; then
-        echo -en "$CYAN\nDownloading files$NC. Please wait...\n\n"
+        echo -en "$CYAN\\nDownloading files$NC. Please wait...\\n\\n"
 
         lftp -c 'open '"$mirrorSource"'; mirror -c -e '"$removeSoure"' '"$onlyPatchesDl"' '"$versionDownload"'/'
         # -c continue a mirror job if possible
@@ -190,19 +197,19 @@ else
 
         listOfFilesAfterDownload=$(find $versionDownload/ -type f -print)
 
-        echo -en "$CYAN\nCreating a$BLUE md5sum$RED (after the download)$CYAN from files (in the folder $GREEN$versionDownload/$CYAN)$NC. Please wait..."
+        echo -en "$CYAN\\nCreating a$BLUE md5sum$RED (after the download)$CYAN from files (in the folder $GREEN$versionDownload/$CYAN)$NC. Please wait..."
         for file in $listOfFilesAfterDownload; do
             md5sum "$file" >> "$tmpMd5sumAfterDownload"
         done
-        echo -e "$CYAN\n\nThe$BLUE md5sum$RED (after the download)$CYAN was saved in the tmp file: $GREEN$tmpMd5sumAfterDownload$NC"
+        echo -e "$CYAN\\n\\nThe$BLUE md5sum$RED (after the download)$CYAN was saved in the tmp file: $GREEN$tmpMd5sumAfterDownload$NC"
 
-        echo -en "$CYAN\nChecking the changes in the files$BLUE before$CYAN with$BLUE after$CYAN download$NC. Please wait..."
+        echo -en "$CYAN\\nChecking the changes in the files$BLUE before$CYAN with$BLUE after$CYAN download$NC. Please wait..."
         changeResult=$(diff -w "$tmpMd5sumBeforeDownload" "$tmpMd5sumAfterDownload")
 
         if [ "$changeResult" == '' ]; then
-            echo -e "$CYAN\nNone changes made in the local folder -$GREEN All files still the same after the download$NC"
+            echo -e "$CYAN\\nNone changes made in the local folder -$GREEN All files still the same after the download$NC"
         else
-            echo -e "$RED\n\nChanges made in local files...$NC"
+            echo -e "$RED\\n\\nChanges made in local files...$NC"
 
             diffBeforeDownload=$(diff -u "$tmpMd5sumBeforeDownload" "$tmpMd5sumAfterDownload" | grep -v "^--" | grep "^-" | awk '{print $2}')
             diffAfterDownload=$(diff -u "$tmpMd5sumBeforeDownload" "$tmpMd5sumAfterDownload" | grep -v "^++" | grep "^+" | awk '{print $2}')
@@ -216,8 +223,8 @@ else
             done
 
             if [ "$filesUpdate" != '' ]; then
-                echo -e "$GREEN\nFile(s) updated:$NC"
-                echo "$filesUpdate" | sed 's/|/\n/g' | sort
+                echo -e "$GREEN\\nFile(s) updated:$NC"
+                echo "$filesUpdate" | sed 's/|/\\n/g' | sort
             fi
 
             for lineA in $diffBeforeDownload; do
@@ -228,8 +235,8 @@ else
             done
 
             if [ "$filesDeleted" != '' ]; then
-                echo -e "$RED\nFile(s) deleted:$NC"
-                echo "$filesDeleted" | sed 's/|/\n/g' | sort
+                echo -e "$RED\\nFile(s) deleted:$NC"
+                echo "$filesDeleted" | sed 's/|/\\n/g' | sort
             fi
 
             for lineB in $diffAfterDownload; do
@@ -240,8 +247,8 @@ else
             done
 
             if [ "$filesNew" != '' ]; then
-                echo -e "$BLUE\nNew file(s) downloaded:$NC"
-                echo "$filesNew" | sed 's/|/\n/g' | sort
+                echo -e "$BLUE\\nNew file(s) downloaded:$NC"
+                echo "$filesNew" | sed 's/|/\\n/g' | sort
             fi
         fi
 
@@ -250,14 +257,14 @@ else
 
     cd "$versionDownload" || exit
 
-    echo -en "$CYAN\nWant check the integrity of downloaded files with the server?$NC\n(y)es - (n)o $GREEN(press enter to yes):$NC "
+    echo -en "$CYAN\\nWant check the integrity of downloaded files with the server?$NC\\n(y)es - (n)o $GREEN(press enter to yes):$NC "
     read -r checkFiles
 
     if [ "$checkFiles" == 'y' ] || [ "$checkFiles" == '' ]; then
-        echo -en "$CYAN\nChecking the integrity of the files$NC. Please wait..."
+        echo -en "$CYAN\\nChecking the integrity of the files$NC. Please wait..."
         checkFilesResult=$(tail +13 CHECKSUMS.md5 | grep -vE "$grepRemove" | md5sum -c --quiet)
 
-        echo -en "$CYAN\n\nFiles integrity:"
+        echo -en "$CYAN\\n\\nFiles integrity:"
         if [ "$checkFilesResult" == '' ]; then
             echo -e "$GREEN Good $BLUE- Files are equal to the server$NC"
         else
@@ -268,7 +275,7 @@ else
 
     cd .. || exit
 
-    echo -en "$CYAN\nWant create a ISO file from downloaded folder?$NC\n(y)es - (n)o $GREEN(press enter to no):$NC "
+    echo -en "$CYAN\\nWant create a ISO file from downloaded folder?$NC\\n(y)es - (n)o $GREEN(press enter to no):$NC "
     read -r generateISO
 
     isoFileName=$versionDownload"_AllPkgs_date_"$(date +%d_%m_%Y)
@@ -277,8 +284,8 @@ else
         olderIsoSlackware=$(ls "slackware*iso")
 
         if [ "$olderIsoSlackware" != '' ]; then
-            echo -e "$CYAN\nOlder ISO file Slackware found:$GREEN $olderIsoSlackware$NC"
-            echo -en "$CYAN\nDelete the older ISO file(s) before continue?$NC\n(y)es - (n)o $GREEN(press enter to no):$NC "
+            echo -e "$CYAN\\nOlder ISO file Slackware found:$GREEN $olderIsoSlackware$NC"
+            echo -en "$CYAN\\nDelete the older ISO file(s) before continue?$NC\\n(y)es - (n)o $GREEN(press enter to no):$NC "
             read -r deleteOlderIso
 
             if [ "$deleteOlderIso" == 'y' ]; then
@@ -286,16 +293,16 @@ else
             fi
         fi
 
-        echo -en "$CYAN\nCreating ISO file.$NC Please wait..."
+        echo -en "$CYAN\\nCreating ISO file.$NC Please wait..."
         mkisofs -pad -r -J -o "$isoFileName.iso" "$versionDownload/"
         # -pad   Pad output to a multiple of 32k (default)
         # -r     Generate rationalized Rock Ridge directory information
         # -J     Generate Joliet directory information
         # -o     Set output file name
 
-        echo -e "$CYAN\n\nThe ISO file $GREEN\"$isoFileName.iso\"$CYAN was generated by the folder $GREEN\"$versionDownload/\"$NC\n"
+        echo -e "$CYAN\\n\\nThe ISO file $GREEN\"$isoFileName.iso\"$CYAN was generated by the folder $GREEN\"$versionDownload/\"$NC\\n"
     else
-        echo -e "$CYAN\nExiting...$GREEN\n\nIf you want create a ISO file, use:"
-        echo -e "${BLUE}mkisofs -pad -r -J -o \"$isoFileName.iso\" \"$versionDownload/\"$NC\n"
+        echo -e "$CYAN\\nExiting...$GREEN\\n\\nIf you want create a ISO file, use:"
+        echo -e "${BLUE}mkisofs -pad -r -J -o \"$isoFileName.iso\" \"$versionDownload/\"$NC\\n"
     fi
 fi
