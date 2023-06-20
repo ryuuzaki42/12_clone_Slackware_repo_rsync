90a6cee6df14897ec794881682c235b24c1fb95a
João Batista <joao42lbatista@gmail.com>
2023-06-20 10:36:30 -0300

    create_ISO_without_some_packages_JBs.sh - \\n to \n \\b to \b \\t to \t

--- create_ISO_without_some_packages_JBs.sh
+++ create_ISO_without_some_packages_JBs.sh
@@ -22,28 +22,28 @@
 #
 # Script: Create a ISO without some package from a local directory that you don't want
 #
-# Last update: 02/06/2023
+# Last update: 19/06/2023
 #
 # Tip: Add the packages you want in the $packagesList
 # Obs.: Need one space before add more
 #
-echo -e "\\nCreate a ISO file from a clone folder of Slackware\\n"
+echo -e "\nCreate a ISO file from a clone folder of Slackware\n"
 
 folderWork=$1
 if [ "$folderWork" == '' ]; then
-    echo -e "Error: You need pass the folder to work\\n"
+    echo -e "Error: You need pass the folder to work\n"
 elif [ ! -d "$folderWork" ]; then
-    echo -e "Error: The directory \"$folderWork\" not exist\\n"
+    echo -e "Error: The directory \"$folderWork\" not exist\n"
 else
     folderWork=${folderWork//\//} # Remove the / in the end
     cd "$folderWork" || exit
 
-    echo -en "ISO with all packages or remove the packages in packagesList?\\n1 to all package - 2 to use packagesList (hit enter to use packagesList): "
+    echo -en "ISO with all packages or remove the packages in packagesList?\n1 to all package - 2 to use packagesList (hit enter to use packagesList): "
     read -r usePackagesList
 
     if [ "$usePackagesList" == '' ] || [ "$usePackagesList" == 2 ]; then
         usePackagesList=2
-        echo -e "\\n # Using \$packagesList #"
+        echo -e "\n # Using \$packagesList #"
 
         ## Add packages that you want in the packagesList
         ## Need one space before add more
@@ -66,7 +66,7 @@ else
         packagesList="$packagesList plasma-vault"
 
         # Remove XFCE or/and KDE
-        echo -en "\\nLeave XFCE or KDE?\\n(1) Leave KDE, (2) Leave XFCE, (3) Remove XFCE and KDE (hit enter to Leave KDE): "
+        echo -en "\nLeave XFCE or KDE?\n(1) Leave KDE, (2) Leave XFCE, (3) Remove XFCE and KDE (hit enter to Leave KDE): "
         read -r leaveXGUI
         if [ "$leaveXGUI" == 1 ] || [ "$leaveXGUI" == '' ]; then
             packagesListTmp="xfce"
@@ -76,7 +76,7 @@ else
             packagesListTmp="kde xfce" # Also remove kde-l10n-
         fi
 
-        echo -e "\\nWill remove \"$packagesListTmp\""
+        echo -e "\nWill remove \"$packagesListTmp\""
         packagesList="$packagesList $packagesListTmp"
 
         # Remove servidor X - Leave fluxbox # Safe propose
@@ -102,18 +102,18 @@ else
         packagesList="$packagesList elementary-xfce gnome-themes-extra xfce4-panel-profiles
         xfce4-screensaver xfce4-whiskermenu-plugin thunar mousepad Greybird"
 
-        echo -e "\\nRemove \"gnome packages\"? \"gcr- polkit-gnome gnome-themes libgnome-keyring gnome-keyring\""
+        echo -e "\nRemove \"gnome packages\"? \"gcr- polkit-gnome gnome-themes libgnome-keyring gnome-keyring\""
         echo "Recommended if you remove XFCE, but leave if you not remove XFCE."
         echo -n "(y)es to remove or (n)ot remove (hit enter to remove): "
         read -r removeGnomePackages
         if [ "$removeGnomePackages" == 'y' ] || [ "$removeGnomePackages" == '' ]; then
             # Remove gnome "packages" # gcr- to not remove libgcrypt
             packagesList="$packagesList gcr- polkit-gnome gnome-themes libgnome-keyring gnome-keyring"
-            echo -en "\\nR"
+            echo -en "\nR"
         else
-            echo -en "\\nNot r"
+            echo -en "\nNot r"
         fi
-        echo -e "emoving \"gnome packages\"\\n"
+        echo -e "emoving \"gnome packages\"\n"
 
         # Remove other packages
         packagesList="$packagesList seamonkey pidgin xchat dragon thunderbird kplayer
@@ -141,7 +141,7 @@ else
         #packagesList="$packagesList kernel-source"
 
         countI=0
-        echo -e "\\nPackages that will be removed:\\n"
+        echo -e "\nPackages that will be removed:\n"
         for packageName in $packagesList; do
             echo -n "$packageName "
             if [ "$countI" == 5 ]; then
@@ -152,10 +152,10 @@ else
             fi
         done
 
-        echo -en "\\n\\nWant continue? (y)es or (n)o: "
+        echo -en "\n\nWant continue? (y)es or (n)o: "
         read -r continueOrNot
         if [ "$continueOrNot" != 'y' ]; then
-            echo -e "\\nJust exiting by local choice\\n"
+            echo -e "\nJust exiting by local choice\n"
             exit 0
         fi
 
@@ -166,18 +166,18 @@ else
         rm "$filesIgnoredInTheISO" "$mkisofsExcludeList" "$filesNotFound" 2> /dev/null
 
         for packageName in $packagesList; do
-            echo -e "\\nLooking for \"$packageName\""
+            echo -e "\nLooking for \"$packageName\""
             resultFind=$(find . | grep "$packageName" | grep -E ".t.z$|.asc$|.txt$")
 
             if [ "$resultFind" == '' ]; then
                 echo "Not found: \"$packageName\"" | tee -a "$filesNotFound"
             else
-                echo -e "Files ignored with the pattern: \"$packageName\"\\n$resultFind\\n" | tee -a "$filesIgnoredInTheISO"
+                echo -e "Files ignored with the pattern: \"$packageName\"\n$resultFind\n" | tee -a "$filesIgnoredInTheISO"
                 echo "$resultFind" | rev | cut -d '/' -f1 | rev >> "$mkisofsExcludeList"
             fi
         done
     else
-        echo -e "\\nUsing all packages"
+        echo -e "\nUsing all packages"
     fi
 
     dateISO=$(date +%d_%b_%Y)
@@ -200,10 +200,10 @@ else
     commandGenISOPart3="-eltorito-alt-boot -no-emul-boot -eltorito-platform 0xEF -eltorito-boot isolinux/efiboot.img \
 ."
 
-    echo -en "\\nWant create a ISO file from work folder?\\n(y)es - (n)o (press enter to no): "
+    echo -en "\nWant create a ISO file from work folder?\n(y)es - (n)o (press enter to no): "
     read -r generateISO
     if [ "$generateISO" == 'y' ]; then
-        echo -e "\\nCreating ISO file. Please wait..."
+        echo -e "\nCreating ISO file. Please wait..."
         if [ "$usePackagesList" == 2 ]; then
             appendMessage="Using packagesList (without some packages)"
             isoFileName="${folderWork}_SelectedPkgs_date_$dateISO"
@@ -212,24 +212,24 @@ else
         fi
 
         commandGenISO=$commandGenISOPart0$commandGenISOPart1$commandGenISOPart2$commandGenISOPart3
-        echo -e "\\nRunning:\\n$commandGenISO\\n"
+        echo -e "\nRunning:\n$commandGenISO\n"
         eval "$commandGenISO"
 
         cd ../ || exit
-        echo -en "\\nCreating md5sum from $isoFileName. Please wait..."
+        echo -en "\nCreating md5sum from $isoFileName. Please wait..."
         md5sum "${isoFileName}.iso" > "${isoFileName}.iso.md5"
 
-        echo -e "\\n\\nThe ISO file: $localISO/$isoFileName.iso\\nWas generated by the folder $appendMessage: $localISO/$folderWork/\\n"
+        echo -e "\n\nThe ISO file: $localISO/$isoFileName.iso\nWas generated by the folder $appendMessage: $localISO/$folderWork/\n"
 
         if [ "$usePackagesList" == 2 ]; then
-            echo -e "Take a look in the files:\\n"
+            echo -e "Take a look in the files:\n"
             echo "$(pwd)/"
-            echo -e "\\t\\t $(echo $mkisofsExcludeList | cut -d '/' -f2)"
-            echo -e "\\t\\t $(echo $filesIgnoredInTheISO | cut -d '/' -f2)"
-            echo -e "\\t\\t $(find "$(echo $filesNotFound | cut -d '/' -f2)" 2> /dev/null | rev | cut -d '/' -f1 | rev)"
+            echo -e "\t\t $(echo $mkisofsExcludeList | cut -d '/' -f2)"
+            echo -e "\t\t $(echo $filesIgnoredInTheISO | cut -d '/' -f2)"
+            echo -e "\t\t $(find "$(echo $filesNotFound | cut -d '/' -f2)" 2> /dev/null | rev | cut -d '/' -f1 | rev)"
         fi
     else
         commandGenISO=$commandGenISOPart0$commandGenISOPart1$commandGenISOPart3
-        echo -e "\\n\\nExiting...\\n\\nIf you want create a ISO file, use:\\n\\ncd $localISO/\\n\\n$commandGenISO\\n"
+        echo -e "\n\nExiting...\n\nIf you want create a ISO file, use:\n\ncd $localISO/\n\n$commandGenISO\n"
     fi
 fi
